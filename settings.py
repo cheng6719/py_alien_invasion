699363c353aa1240ac75d129b4b93e53a27849bc
cheng6719 <cheng6719@126.com>
2022-05-28 21:45:13 +0800

    update

--- settings.py
+++ settings.py
@@ -17,14 +17,14 @@ class Settings:
         
         # 子弹设置
         self.bullet_speed = 1.0
-        self.bullet_width = 300
+        self.bullet_width = 30
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
         
         # 外星人设置
-        #self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 0.1
+        self.fleet_drop_speed = 1
         # 1 向右，-1 向左
         #self.fleet_direction = 1
         
@@ -43,7 +43,7 @@ class Settings:
         """初始化随游戏进行而变化的设置"""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = 0.1
         
         # fleet_direction 为 1 向右，-1 向左
         self.fleet_direction = 1
